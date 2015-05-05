23e175b88d71f21436326205d9356cee627afa66
itamar sharify <itamarsharify@gmail.com>
2015-05-05 10:33:36 +0300

    Added Person Search Class with Unittest

---
+++ PersonSearch.py
@@ -0,0 +1,17 @@
+__author__ = 'itamar'
+import unittest
+
+class CrowdMap (object):
+    def get_all_post(self,param):
+     pass
+
+class PersonSearch (unittest.TestCase):
+    def setUp(self):
+        self.crowdmap = CrowdMap()
+
+    def test_getAllPostsForName(self):
+        posts = self.crowdmap.get_all_post_for("Or")
+        self.assertIn("Or",posts)
+
+if __name__ == '__main__':
+    unittest.main()
