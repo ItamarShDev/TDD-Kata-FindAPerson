a88e16f3ec9fa0c9ba8800a41a6c35ec42b44e8c
itamar sharify <itamarsharify@gmail.com>
2015-05-19 10:20:21 +0300

    RED: check map incosistencies

--- crowdmap.py
+++ crowdmap.py
@@ -2,12 +2,16 @@ __author__ = 'itamar'
 
 class CrowdMap():
 
-    def get_all_post_for(self,name):
+    def get_all_post_namefor(self):
         return ["Or"]
+
     def geo_location_existed(self,name):
         posts = self.get_all_post_for(name)
         for loc in posts:
             if loc == "Bangkok":
                 return True
 
+        return False
+
+    def check_for_incosistencies(self):
         return False
