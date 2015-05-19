8689210898f2817c669d73ab96f61c9df0f35bf2
itamar sharify <itamarsharify@gmail.com>
2015-05-19 10:21:11 +0300

    REFACTOR: check if  existed on map

--- PersonSearch.py
+++ PersonSearch.py
@@ -21,7 +21,7 @@ class PersonSearch (unittest.TestCase):
     #Given a name, check if the map includes a location information (place or geo. location)
     def test_if_map_includes_location(self):
         reply = self.crowdmap.geo_location_existed("Or")
-
+        return reply
     #Check if there are map inconsistencies, e.g., the same name with different locations.
     def test_for_map_incosistencies(self):
         self.crowdmap.check_for_incosistencies()
